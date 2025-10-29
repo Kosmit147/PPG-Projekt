ebff0f89c93e2456d37a6cd189eab166d6ea9e82
Wojciech Opara <wwojtek660@gmail.com>
2025-10-29 20:33:09 +0100

    Clamp camera pitch

--- Assets/Scripts/FpsPlayer.cs
+++ Assets/Scripts/FpsPlayer.cs
@@ -7,6 +7,8 @@ public class FpsPlayer : MonoBehaviour
     [Header("Input")]
     public InputActionProperty lookAction;
     public float lookSpeed = 0.1f;
+    public float minPitch = -89.0f;
+    public float maxPitch = 89.0f;
 
     private Vector2 eulerAngles = Vector2.zero;
 
@@ -19,10 +21,10 @@ public class FpsPlayer : MonoBehaviour
     {
         var look = lookSpeed * lookAction.action.ReadValue<Vector2>();
 
-        eulerAngles.x += look.y;
+        eulerAngles.x = Mathf.Clamp(eulerAngles.x - look.y, -maxPitch, -minPitch);
         eulerAngles.y += look.x;
 
-        var rotationX = Quaternion.AngleAxis(eulerAngles.x, Vector3.left);
+        var rotationX = Quaternion.AngleAxis(eulerAngles.x, Vector3.right);
         var rotationY = Quaternion.AngleAxis(eulerAngles.y, Vector3.up);
 
         transform.rotation = (rotationY * rotationX).normalized;
