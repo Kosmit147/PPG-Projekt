7e57b2ba490e2b3ebc32b02d9f64dcc23d34850a
Wojciech Opara <wwojtek660@gmail.com>
2025-10-30 22:13:59 +0100

    Extract fps camera functionality into a separate script

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,57 +1,31 @@
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
-    public GameObject playerCamera = null;
+    public new GameObject camera = null;
 
-    [Header("Input")]
     public InputActionProperty moveAction; // Expects a Vector2.
-    public InputActionProperty lookAction; // Expects a Vector2.
     public InputActionProperty jumpAction; // Expects a button.
     public float moveSpeed = 2.0f;
-    public float lookSpeed = 0.1f;
     public float jumpForce = 5.0f;
-    public float minPitch = -89.0f;
-    public float maxPitch = 89.0f;
 
     public float gravity = -9.81f;
 
-    private Vector2 cameraEulerAngles = Vector2.zero;
     private CharacterController characterController = null;
     private float verticalVelocity = 0.0f;
 
     void Start()
     {
-        Cursor.lockState = CursorLockMode.Locked;
-        cameraEulerAngles = transform.eulerAngles;
-        characterController = gameObject.GetComponent<CharacterController>();
+        characterController = gameObject.GetOrAddComponent<CharacterController>();
     }
 
     void Update()
     {
-        UpdateCamera();
-        Move();
-    }
-
-    void Move()
-    {
         // TODO: Make sure to set transform.forward;
         // TODO: Sprinting.
 
-        UpdateVerticalVelocity();
-
-        var moveInput = Vector2.ClampMagnitude(moveAction.action.ReadValue<Vector2>(), 1.0f);
-        var moveRotation = Quaternion.AngleAxis(cameraEulerAngles.y, Vector3.up);
-
-        var motion = moveRotation * new Vector3(moveInput.x, 0.0f, moveInput.y) * moveSpeed;
-        motion.y = verticalVelocity;
-
-        characterController.Move(Time.deltaTime * motion);
-    }
-
-    void UpdateVerticalVelocity()
-    {
         if (characterController.isGrounded)
         {
             if (verticalVelocity < 0.0f)
@@ -62,19 +36,13 @@ public class Player : MonoBehaviour
         }
 
         verticalVelocity += Time.deltaTime * gravity;
-    }
 
-    void UpdateCamera()
-    {
-        var look = lookSpeed * lookAction.action.ReadValue<Vector2>();
+        var moveInput = Vector2.ClampMagnitude(moveAction.action.ReadValue<Vector2>(), 1.0f);
+        var moveRotation = Quaternion.AngleAxis(camera.transform.eulerAngles.y, Vector3.up);
 
-        cameraEulerAngles.x -= look.y;
-        cameraEulerAngles.y += look.x;
-        cameraEulerAngles.x = Mathf.Clamp(cameraEulerAngles.x, -maxPitch, -minPitch);
+        var motion = moveRotation * new Vector3(moveInput.x, 0.0f, moveInput.y) * moveSpeed;
+        motion.y = verticalVelocity;
 
-        var rotationX = Quaternion.AngleAxis(cameraEulerAngles.x, Vector3.right);
-        var rotationY = Quaternion.AngleAxis(cameraEulerAngles.y, Vector3.up);
-
-        playerCamera.transform.rotation = (rotationY * rotationX).normalized;
+        characterController.Move(Time.deltaTime * motion);
     }
 }
---
+++ todo.md
@@ -0,0 +1,12 @@
+﻿# Lab 1
+1. FPS Camera. ✓
+2. Movement with jumping and crouching.
+3. Collisions with environment. ✓
+4. Top-down camera.
+5. Switching between FPS Camera and top-down camera.
+6. Player animations.
+7. Top-down camera movement.
+8. Top-down camera zoom.
+9. Player movement with mouse when using top-down camera.
+
+# Lab 2
